b44df8aa2249ae805eb2bfaf836bf8a23b0e2492
puamingfei <67616872+puamingfei@users.noreply.github.com>
2021-06-30 18:19:16 +0800

    Update FruitRepository.java

--- spring/spring-rest-services/src/main/java/com/example/service/FruitRepository.java
+++ spring/spring-rest-services/src/main/java/com/example/service/FruitRepository.java
@@ -12,8 +12,30 @@ public class FruitController {
     @Autowired FruitRepository repository; // Ignore this
 
     // TODO GET mappings
+        @GetMapping
+        public List<Fruit> getAll() {
+            return repository.findAll();
+        }
+
+        @GetMapping("/{id}")
+        public Fruit getFruit(@PathVariable("id") Long id) {
+            return repository.findById(id).orElse(null);
+        }
     // TODO POST mapping
+            @PostMapping
+        public Fruit createFruit(@RequestBody Fruit fruit) {
+            return repository.save(fruit);
+        }
     // TODO PUT mapping
+            @PutMapping("/{id}")
+        public Fruit updateFruit(@PathVariable("id") Long id, @RequestBody Fruit fruit) {
+            fruit.setId(id);
+            return repository.save(fruit);
+        }
     // TODO DELETE mapping
+            @DeleteMapping("/{id}")
+        public void delete(@PathVariable("id") Long id) {
+            repository.deleteById(id);
+        }
 
 }
