9616d85075a89115f2b78ad8c837e2223ba85580
puamingfei <67616872+puamingfei@users.noreply.github.com>
2021-06-30 18:59:30 +0800

    Update FruitRepository.java

--- spring/spring-rest-services/src/main/java/com/example/service/FruitRepository.java
+++ spring/spring-rest-services/src/main/java/com/example/service/FruitRepository.java
@@ -1,41 +1,8 @@
 package com.example.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/fruits")
-public class FruitController {
-
-    @Autowired FruitRepository repository; // Ignore this
-
-    // TODO GET mappings
-        @GetMapping
-        public List<Fruit> getAll() {
-            return repository.findAll();
-        }
-
-        @GetMapping("/{id}")
-        public Fruit getFruit(@PathVariable("id") Long id) {
-            return repository.findById(id).orElse(null);
-        }
-    // TODO POST mapping
-            @PostMapping
-        public Fruit createFruit(@RequestBody Fruit fruit) {
-            return repository.save(fruit);
-        }
-    // TODO PUT mapping
-            @PutMapping("/{id}")
-        public Fruit updateFruit(@PathVariable("id") Long id, @RequestBody Fruit fruit) {
-            fruit.setId(id);
-            return repository.save(fruit);
-        }
-    // TODO DELETE mapping
-            @DeleteMapping("/{id}")
-        public void delete(@PathVariable("id") Long id) {
-            repository.deleteById(id);
-        }
-
+@Repository
+public interface FruitRepository extends JpaRepository<Fruit, Long> {
 }
